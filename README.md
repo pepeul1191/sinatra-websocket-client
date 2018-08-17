4b2fa20b9bcf39fd5255bc92487be2374e0a48c1
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-16 20:02:44 -0500

    Autoload de todos los archivos en ./routes

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Ejecutar aplicación:
 Fuentes:
 
 + https://blog.carbonfive.com/2013/06/24/sinatra-best-practices-part-one/
++ https://stackoverflow.com/questions/735073/best-way-to-require-all-files-from-a-directory-in-ruby
--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
-require_relative 'routes/home'
+#require_relative 'routes/home'
 
 class App < Sinatra::Base
+  #configuraciones
   set :root, File.dirname(__FILE__)
   enable :sessions
+  #before all requests
+  before do
+    headers['server'] = 'Ruby, Ubuntu'
+  end
+  #registro de rutas
+  Dir["./routes/*.rb"].each {|file| require file }
   #registro de rutas
   register Sinatra::App::Routing::Home
 end
