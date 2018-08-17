fcdf25936f70085daa009bd241aa3007bffdc845
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-16 21:15:41 -0500

    Bower añadido

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 CONSTANTS = {
   :base_url => 'http://localhost:3000/',
-  :static_url => 'http://localhost:9090/',
+  :static_url => 'http://localhost:3000/',
   :csrf => {
     :secret => 'mpt/sr6eS2AlCRHU7DVThMgFTN08pnfSDf/C94eZx7udfm0lvgaYWLYJttYPKzGKDTlXwVU/d2FOxbKkgNlsTw==',
     :key => 'Csrf_Val',
